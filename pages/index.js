e5bb0737cb0a01d2bd181051b750c809215e8947
ozaki <fs65ugumori@yahoo.co.jp>
2019-11-12 08:16:28 +0900

    update index page

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,31 @@
-import React from "react";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 import Layout from "../components/MyLayout";
 
-function PostLink({ id }) {
-  return (
-    <li>
-      <Link href="/p/[id]" as={`/p/${id}`}>
-        <a>{id}</a>
-      </Link>
-    </li>
-  );
-}
-
-function Blog() {
+function Index({ shows }) {
   return (
     <Layout>
-      <h1>My Blog</h1>
+      <h1>Batman TV Shows</h1>
       <ul>
-        <PostLink id="Hello Next.js" />
-        <PostLink id="Learn Next.js is awesome" />
-        <PostLink id="Deploy apps with Zeit" />
+        {shows.map(show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </Layout>
   );
 }
 
-export default Blog;
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+  console.log(`Show data fetched. Count: ${data.length}`);
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
