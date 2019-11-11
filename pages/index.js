e649247d6cc0010f899320211996a342487c42a7
ozaki <fs65ugumori@yahoo.co.jp>
2019-11-12 07:40:40 +0900

    add Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 function Index() {
   return (
     <div>
+      <Link href="/about">
+        <a>About Page</a>
+      </Link>
       <p>Hello Next.js</p>
     </div>
   );
