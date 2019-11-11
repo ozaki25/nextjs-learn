b475c51d41d0fbf05b704b8016c46e64f3870c1c
ozaki <fs65ugumori@yahoo.co.jp>
2019-11-12 07:50:13 +0900

    add Blog list

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,27 @@ import React from "react";
 import Link from "next/link";
 import Layout from "../components/MyLayout";
 
-function Index() {
+function PostLink({ title }) {
+  return (
+    <li>
+      <Link href={`/post?title=${title}`}>
+        <a>{title}</a>
+      </Link>
+    </li>
+  );
+}
+
+function Blog() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   );
 }
 
-export default Index;
+export default Blog;
