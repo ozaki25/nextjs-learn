ac407fa4a16119dc46d9736dae23f6c96c40a6d2
ozaki <fs65ugumori@yahoo.co.jp>
2019-11-12 08:19:33 +0900

    update post page

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,18 +1,25 @@
 import { useRouter } from "next/router";
 import Layout from "../../components/MyLayout";
 
-function Post() {
+function Post({ show }) {
   const router = useRouter();
-  const {
-    query: { id }
-  } = router;
+  const { name, summary, image } = show;
 
   return (
     <Layout>
-      <h1>{id}</h1>
-      <p>This is the blog post content.</p>
+      <h1>{name}</h1>
+      <p>{summary.replace(/<[/]?[pb]>/g, "")}</p>
+      <img src={image.medium} />
     </Layout>
   );
 }
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+  console.log(`Fetched show: ${show.name}`);
+  return { show };
+};
 
 export default Post;
